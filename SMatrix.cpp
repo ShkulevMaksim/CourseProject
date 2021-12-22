c65a2d3c97cd0d79bfc7eb9ba8f8a78bcfd4fc5d
ShkulevMaksim <79005304+ShkulevMaksim@users.noreply.github.com>
2021-12-23 03:10:20 +0700

    Готово!

--- SMatrix.cpp
+++ SMatrix.cpp
@@ -1,5 +1,5 @@
 //
-// Created by mksh2 on 21.11.2021.
+// Created by Maksim Shkulev on 21.11.2021.
 //
 
 #include "SMatrix.h"
@@ -7,54 +7,41 @@
 #include <stdexcept>
 
 
-SMatrix::SMatrix(): size_x(0),size_y(0){}
 
-SMatrix::SMatrix(int x, int y, const std::list<int>& numbers):size_x(x),size_y(y) {
-        matrix_element element;
+SMatrix::SMatrix(): size_x(0),size_y(0){}
+SMatrix::SMatrix(int y, int x):size_x(x),size_y(y) {}
+
+SMatrix::SMatrix(int y, int x, const std::list<int>& numbers):size_x(x),size_y(y) {
+    try{
+        if(numbers.size() != size_x*size_y){throw MyException("Wrong dimensions");}
         int i=0;
         int j=0;
-            for(auto it=numbers.begin(); it!=numbers.end();++it){
-                if(*it!=0){
-                    element.pos_x=i;
-                    element.pos_y=j;
-                    element.value=*it;
-                    matrix.push_back(element);
-
-                }
-                i++;
-                if(i>=(size_x)){
-                    i=0;
-                    j++;
-                }
-
+        for(auto it=numbers.begin(); it!=numbers.end();++it){
+            matrix.emplace_back(j,i,*it);
+            i++;
+            if(i>=size_x){
+                i=0;
+                j++;
             }
+        }
+    }
+    catch(const MyException& ex){
+        std::cout<<"Exception is: "<<ex.what()<<'\n';
+    }
 
 }
-
 
 
 SMatrix::SMatrix(SMatrix &matrix_copy) {
     size_x=matrix_copy.size_x;
     size_y=matrix_copy.size_y;
-    matrix= matrix_copy.matrix;
+   matrix= matrix_copy.matrix;
 }
 
 SMatrix::~SMatrix() = default;
 
 
-void SMatrix::print() {
-    auto it=matrix.begin();
-    for (int j = 0; j < size_y; ++j) {
-        for (int i=0;i<size_x; ++i){
-           if( (it->pos_x==i) && (it->pos_y==j)){
-               std::cout<<it->value<<"\t";
-               ++it;
-           }
-           else {  std::cout<<"0\t";}
-        }
-        std::cout<<std::endl;
-    }
-}
+
 
 SMatrix &SMatrix::operator=(const SMatrix& m2) {
     size_x = m2.size_x;
@@ -63,38 +50,259 @@ SMatrix &SMatrix::operator=(const SMatrix& m2) {
     return *this ;
 }
 
-SMatrix &SMatrix::operator+(SMatrix &mat) {
-    std::list<matrix_element>::iterator it_1;
-    if(size_x == mat.size_x && size_y == mat.size_y){
-        auto it1=matrix.begin();
-        auto it2=mat.matrix.begin();
-        for (int j = 0; j < size_y; ++j) {
-            for (int i=0;i<size_x; ++i){
-                if(i==it1->pos_x && j == it1->pos_y && it2->pos_x!=i && it2->pos_y!=j){
-                    it1++;
-                    continue;
-                }
-                if(i==it1->pos_x && j == it1->pos_y && it2->pos_x==i && it2->pos_y==j){
-                    it1->value+=it2->value;
-                    it1++;
-                    it2++;
-                    continue;
-                }
-                if(i!=it1->pos_x && j != it1->pos_y && it2->pos_x==i && it2->pos_y==j){
-                    matrix.insert(it1,*it2);
-                    it2++;
-                    continue;
-                }
+
+int SMatrix::operator()(int y, int x) const {
+    int val=0;
+    for(auto it=matrix.begin();it!=matrix.end();++it){
+        if(it->pos_x==x && it->pos_y==y) {val= it->value; break;}
+    }
+    return val;
+}
+
+
+int &SMatrix::operator()(int y, int x) {
+
+    for(auto it=matrix.begin();it!=matrix.end();++it){
+        if(it->pos_x==x && it->pos_y==y) {
+          return it->value;
+        }
+    }
+    matrix.emplace_back(y,x,NULL);
+    return this->operator()(y,x);
+
+}
 
 
 
+void SMatrix::print() const {
+    try{
+        if(size_x==0 || size_y==0){std::cout<<"Matrix is empty"<<'\n'; return;}
 
+        for (int i=0;i<size_y; ++i) {
+            for (int j = 0; j < size_x; ++j){
+                std::cout<<this->operator()(i,j)<<"\t";
+            }
+            std::cout<<'\n';
+        }
+        std::cout<<'\n';
+    }
+    catch(const MyException& ex){
+        std::cout<<"Exception is: "<<ex.what()<<'\n';
+    }
+}
+
+SMatrix SMatrix::operator+(const SMatrix &mat) {
+    try{
+        if(!this->sameSize(mat)){throw MyException("Dimensions dont match");}
+        SMatrix buffer(*this);
+        for(int j=0;j<size_y;j++){
+            for(int i=0;i<size_x;i++){
+                buffer(i,j)+=mat(i,j);
             }
         }
+        return buffer ;
+    }
+    catch(const MyException& ex){
+        std::cout<<"Exception is: "<<ex.what()<<'\n';
+    }
+}
 
+SMatrix SMatrix::operator-(const SMatrix &mat) {
+    try{
+        if(!this->sameSize(mat)){throw MyException("Dimensions dont match");}
+        SMatrix buffer(*this);
+        for(int j=0;j<size_y;j++){
+            for(int i=0;i<size_x;i++){
+                buffer(i,j)-=mat(i,j);
+            }
+        }
+        return buffer ;
+    }
+    catch(const MyException& ex){
+        std::cout<<"Exception is: "<<ex.what()<<'\n';
+    }
+
+}
+
+SMatrix SMatrix::operator*(const SMatrix &mat) {
+    try{if(this->size_x!=mat.size_y){throw MyException("Wrong dimensions for finding product");}
+        SMatrix buffer(this->size_y,mat.size_x);
+        int sum =0;
+        for(int i=0;i<size_y;i++){
+            for(int j=0;j<mat.size_x;j++){
+                sum =0;
+                for(int k=0;k<size_x;k++){
+                    sum+= this->operator()(i,k)*mat(k,j);
+                }
+                buffer(i,j)=sum;
+            }
+        }
+        return buffer;}
+
+    catch(const MyException& ex){
+        std::cout<<"Exception is: "<<ex.what()<<'\n';
+    }
+
+
+
+
+}
+
+bool SMatrix::sameSize(const SMatrix &mat) const {
+    return (mat.size_x==size_x && mat.size_y== size_y);
+}
+
+SMatrix SMatrix::operator++() {
+    for(auto it=matrix.begin();it!=matrix.end();++it){
+        it->value++;
     }
     return *this;
 }
+
+SMatrix SMatrix::operator--() {
+    for(auto it=matrix.begin();it!=matrix.end();++it){
+        it->value--;
+    }
+    return *this;
+}
+
+const SMatrix SMatrix::operator++(int) {
+    SMatrix temp;
+    for(auto it=matrix.begin();it!=matrix.end();++it){
+        it->value++;
+    }
+    return temp;
+}
+
+const SMatrix SMatrix::operator--(int) {
+    SMatrix temp;
+    for(auto it=matrix.begin();it!=matrix.end();++it){
+        it->value--;
+    }
+    return temp;
+}
+
+
+
+SMatrix SMatrix::operator*(int num) {
+    for(auto it=matrix.begin();it!=matrix.end();++it){
+        it->value*=num;
+    }
+    return *this;
+}
+
+SMatrix operator*(int num, SMatrix &mat) {
+    for(auto it=mat.matrix.begin();it!=mat.matrix.end();++it){
+        it->value*=num;
+    }
+    return mat;
+}
+
+bool SMatrix::operator<(const SMatrix &rhs) const {
+    if (size_y < rhs.size_y && size_x < rhs.size_x)
+        return true;
+    else
+        return false;
+}
+
+bool SMatrix::operator>(const SMatrix &rhs) const {
+    return rhs < *this;
+}
+
+bool SMatrix::operator<=(const SMatrix &rhs) const {
+    return !(rhs < *this);
+}
+
+bool SMatrix::operator>=(const SMatrix &rhs) const {
+    return !(*this < rhs);
+}
+
+
+bool SMatrix::operator==(const SMatrix &rhs) const {
+    for (auto it=matrix.begin();it!=matrix.end();++it){
+        if(it->value!=rhs(it->pos_y,it->pos_x)){
+            return false;
+        }
+    }
+    return size_y == rhs.size_y &&
+           size_x == rhs.size_x ;
+}
+
+bool SMatrix::operator!=(const SMatrix &rhs) const {
+    return !(rhs == *this);
+}
+
+int SMatrix::getSizeY() const {
+    return size_y;
+}
+
+int SMatrix::getSizeX() const {
+    return size_x;
+}
+
+SMatrix SMatrix::transpose() {
+    int t;
+    for(int i = 0; i < size_y; ++i)
+    {
+        for(int j = i; j < size_x; ++j)
+        {
+            t = this->operator()(i,j);
+            this->operator()(i,j) = this->operator()(j,i);
+            this->operator()(j,i) = t;
+        }
+    }
+    return *this;
+}
+
+SMatrix SMatrix::operator+=(int num) {
+     for(auto it=matrix.begin();it!=matrix.end();++it){
+        it->value+=num;
+    }
+    return *this;
+}
+
+SMatrix SMatrix::operator-=(int num) {
+    for(auto it=matrix.begin();it!=matrix.end();++it){
+        it->value-=num;
+    }
+    return *this;
+}
+
+SMatrix SMatrix::operator*=(int num) {
+    for(auto it=matrix.begin();it!=matrix.end();++it){
+        it->value*=num;
+    }
+    return *this;
+}
+
+SMatrix operator+(int num, SMatrix &mat) {
+    for(auto it=mat.matrix.begin();it!=mat.matrix.end();++it){
+        it->value+=num;
+    }
+    return mat;
+}
+
+SMatrix SMatrix::operator+(int num) {
+    for(auto it=matrix.begin();it!=matrix.end();++it){
+        it->value+=num;
+    }
+    return *this;
+}
+
+int **SMatrix::toArray() const {
+    int **arr= new int*[size_y];
+    for(auto i=0;i<size_y;i++){
+        arr[i]= new int[size_x];
+        for(int j = 0; j < size_x; j++){ arr[i][j] = this->operator()(i,j); }
+    }
+    return arr;
+}
+
+
+
+
+
+
 
 
 
